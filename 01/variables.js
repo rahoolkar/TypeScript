ef025ef4def7d7cac8d2d0a9d380f43f91a48bc8
rahoolkar <batman30jan2002@gmail.com>
2025-06-17 17:21:44 +0530

    variables in ts

---
+++ 01/variables.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Rahul";
+console.log(name.toUpperCase());
+var age = 13.78;
+console.log(age.toFixed(1));
---
+++ 01/variables.ts
@@ -0,0 +1,6 @@
+const name: string = "Rahul";
+console.log(name.toUpperCase());
+
+const age = 13.78;
+console.log(age.toFixed(1));
+export {};
