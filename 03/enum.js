82d0a2c7b87a6c51ba4b66996cb96eb5d91f4437
rahoolkar <batman30jan2002@gmail.com>
2025-06-18 03:37:34 +0530

    enum

---
+++ 03/enum.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var movieToWatchToday = 101 /* movieCategory.horror */;
+console.log(movieToWatchToday);
---
+++ 03/enum.ts
@@ -0,0 +1,11 @@
+const enum movieCategory {
+  "horror" = 101,
+  "funny",
+  "sci-fi",
+}
+
+const movieToWatchToday = movieCategory.horror;
+
+console.log(movieToWatchToday);
+
+export {};
