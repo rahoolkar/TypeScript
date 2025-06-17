41535d624b1a62b7147fa3e0475742394dba0925
rahoolkar <batman30jan2002@gmail.com>
2025-06-18 02:29:31 +0530

    union

---
+++ 03/union.js
@@ -0,0 +1,8 @@
+//union with variables
+var score;
+score = 89;
+score = "A+";
+console.log(score);
+//union with arrays
+var marks = ["A", 89, "B", 67];
+console.log(marks);
---
+++ 03/union.ts
@@ -0,0 +1,15 @@
+//union with variables
+
+let score: number | string;
+
+score = 89;
+
+score = "A+";
+
+console.log(score);
+
+//union with arrays
+
+const marks: (string | number)[] = ["A", 89, "B", 67];
+
+console.log(marks);
