7f3a1d3964c18ec7fd4b4c5908457cc0d1fcf086
rahoolkar <batman30jan2002@gmail.com>
2025-06-18 01:57:57 +0530

    arrays

---
+++ 03/arrays.js
@@ -0,0 +1,9 @@
+//arrays
+//definfing an empty array
+var emtArray = [];
+//emtArray.push(1) // we cannot push items to empty array
+//array of strings
+var names = [];
+names.push("rahul");
+names.push("udayan");
+console.log(names);
---
+++ 03/arrays.ts
@@ -0,0 +1,14 @@
+//arrays
+
+//definfing an empty array
+const emtArray: [] = [];
+//emtArray.push(1) // we cannot push items to empty array
+
+//array of strings
+const names: string[] = [];
+names.push("rahul");
+names.push("udayan");
+
+console.log(names);
+
+export {};
