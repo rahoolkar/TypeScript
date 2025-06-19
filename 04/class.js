2db537dc8385f90a0d6bf107648e517f78fe4c37
rahoolkar <batman30jan2002@gmail.com>
2025-06-19 18:06:19 +0530

    getter setter

--- 04/class.js
+++ 04/class.js
@@ -2,11 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var User = /** @class */ (function () {
     function User(name, age) {
+        this.password = "1234";
         this.name = name;
         this.age = age;
     }
+    Object.defineProperty(User.prototype, "UserPassword", {
+        get: function () {
+            return this.password;
+        },
+        set: function (password) {
+            this.password = password;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return User;
 }());
 var rahulUser = new User("rahul", 23);
+
 rahulUser.city = "rohtak";
+
+var password = rahulUser.UserPassword; //getter
+rahulUser.UserPassword = "abcd"; //setter
+
 console.log(rahulUser);
+console.log(password);
--- 04/class.ts
+++ 04/class.ts
@@ -1,16 +1,30 @@
 class User {
-  name: string;
-  age: number;
-  city: string;
+  public name: string;
+  public age: number;
+  public city: string;
+  private password: string = "1234";
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
+  }
+
+  get UserPassword(): string {
+    return this.password;
+  }
+
+  set UserPassword(password: string) {
+    this.password = password;
   }
 }
 
 let rahulUser = new User("rahul", 23);
 rahulUser.city = "rohtak";
 
+let password = rahulUser.UserPassword; //getter
+
+rahulUser.UserPassword = "abcd" //setter
+
 console.log(rahulUser);
+console.log(password);
 
 export {};
